35724ce6bc13efbbe5797be157c90db6a78b940c
Ltn Smart <tongtrang136109@gmail.com>
2025-10-05 13:02:48 +0700

    vite.config.ts
    Signed-off-by: Ltn Smart <tongtrang136109@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // quan trọng cho Netlify
+})
       server: {
         port: 3000,
         host: '0.0.0.0',
